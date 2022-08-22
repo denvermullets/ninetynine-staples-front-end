4e0afc50a061f920c7fe56bb89d36edda3900e1f
denvermullets <devmullets@gmail.com>
2022-08-22 07:02:20 -0400

    fix(magicCard): remove temp data

--- src/components/MagicCard/index.tsx
+++ src/components/MagicCard/index.tsx
@@ -8,16 +8,6 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 
-const data = {
-  isNew: true,
-  imageURL:
-    "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
-  name: "Wayfarer Classic",
-  price: 4.5,
-  rating: 4.2,
-  numReviews: 34,
-};
-
 interface MagicCardProps {
   id: string;
   name: string;
@@ -38,7 +28,7 @@ const MagicCard: React.FC<MagicCardProps> = (props) => {
         shadow="lg"
         position="relative"
       >
-        {data.isNew && (
+        {
           <Circle
             size="10px"
             position="absolute"
@@ -46,17 +36,17 @@ const MagicCard: React.FC<MagicCardProps> = (props) => {
             right={2}
             bg="red.200"
           />
-        )}
+        }
 
         <Image src={image_medium} alt={`Picture of ${name}`} roundedTop="lg" />
 
         <Box p="6">
           <Box alignItems="baseline">
-            {data.isNew && (
+            {
               <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
                 New
               </Badge>
-            )}
+            }
           </Box>
           <Flex mt="1" justifyContent="space-between" alignContent="center">
             <Box
