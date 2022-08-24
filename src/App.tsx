a2d61d63b823edf7c06536d275d12c937f3bef37
denvermullets <devmullets@gmail.com>
2022-08-24 08:03:03 -0400

    feat(navbar): add navbar template, add react-router, nin-19

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
+import AppContainer from "./components/AppContainer";
+import { Navbar } from "./components/NavBar";
 import LandingPage from "./views/Landing";
 
+const useStyles = createUseStyles(() => ({
+  root: {
+    width: "100%",
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <AppContainer>
+      <div className={classes.root}>
+        <Navbar />
+        <LandingPage />
+      </div>
+    </AppContainer>
   );
 }
 
