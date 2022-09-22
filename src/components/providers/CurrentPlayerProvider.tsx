fed9a679f37c6392995dd38577e387b05cf009fd
denvermullets <devmullets@gmail.com>
2022-09-21 20:11:06 -0400

    fix(cookies): disable cookies

--- src/components/providers/CurrentPlayerProvider.tsx
+++ src/components/providers/CurrentPlayerProvider.tsx
@@ -24,20 +24,20 @@ export const PlayerContext = createContext(null);
 
 export const CurrentPlayerProvider = ({ children }) => {
   const [currentPlayer, setCurrentPlayer] = useState<CurrentPlayer>();
-  const [cookies] = useCookies(["ninetynine_staples"]);
+  // const [cookies] = useCookies(["ninetynine_staples"]);
 
-  useEffect(() => {
-    if (cookies.ninetynine_staples && !currentPlayer) {
-      setCurrentPlayer({
-        email: cookies.ninetynine_staples.email,
-        created_at: cookies.ninetynine_staples.created_at,
-        updated_at: cookies.ninetynine_staples.updated_at,
-        id: cookies.ninetynine_staples.id,
-        username: cookies.ninetynine_staples.username,
-        token: cookies.ninetynine_staples.token,
-      });
-    }
-  }, [cookies]);
+  // useEffect(() => {
+  //   if (cookies.ninetynine_staples && !currentPlayer) {
+  //     setCurrentPlayer({
+  //       email: cookies.ninetynine_staples.email,
+  //       created_at: cookies.ninetynine_staples.created_at,
+  //       updated_at: cookies.ninetynine_staples.updated_at,
+  //       id: cookies.ninetynine_staples.id,
+  //       username: cookies.ninetynine_staples.username,
+  //       token: cookies.ninetynine_staples.token,
+  //     });
+  //   }
+  // }, [cookies]);
 
   return (
     <PlayerContext.Provider value={{ currentPlayer, setCurrentPlayer }}>
